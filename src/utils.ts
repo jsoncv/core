800c04cd851b1cda0ac4915a91e0621ef9f089ba
Amin Zabardast <zabardast.amin@metu.edu.tr>
2021-11-05 10:07:39 +0300

    Added Comments - Self Documentation - added `build` command #9

--- src/utils.ts
+++ src/utils.ts
@@ -2,12 +2,14 @@ import { cwd } from 'process'
 import axios from 'axios'
 import fs from 'fs'
 
+// Checking to see if a string is a valid URL
 const isUrlRegex = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/gi);
-
 const isUrl = (location:string) => location.match(isUrlRegex)
 
+// Checking if a location path is absolute or not
 const isLocationAbsolute = (location:string):boolean => location.startsWith('/')
 
+// Loading files from URL's or Local Location
 // FIXME: Return any is not good
 const loadFile = (location:string):any => {
     return new Promise((resolve, reject) => {
